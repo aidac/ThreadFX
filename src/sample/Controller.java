af9c2cf863d9071cb12b2cf5cf9e75105dd8e672
aidas <aidas@bmx.lt>
2017-02-02 19:50:30 +0200

    initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,56 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Window;
+
+import java.awt.*;
+import java.io.File;
+import java.io.PrintWriter;
+
+public class Controller {
+
+    public ProgressBar progressBar;
+    public Label headerText;
+    public Label saveText;
+    public Label countText;
+    public TextArea textArea;
+    public Button saveButton;
+    public Button countButton;
+    public Window stage;
+    public Desktop desktop;
+
+
+    public void setSaveButtonOnClick(ActionEvent actionEvent) {
+        saveText.setText("File saved");
+        String contents = textArea.getText();
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            try (PrintWriter out = new PrintWriter(file)) {
+                out.println(contents);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public void setCountButtonOnClick(ActionEvent actionEvent) {
+        Counter cnt = new Counter();
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            countText.setText("the count is: " + cnt.count(file));
+        }
+        double progress = 0.0;
+        for (double i = 0.0; i < 1000000.0; i++) {
+            i = progress;
+            progress++;
+        }
+        progressBar.setProgress(progress / 10000);
+    }
+}
---
+++ src/sample/Counter.java
@@ -0,0 +1,33 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+/**
+ * Created by aidas on 31/01/2017.
+ */
+public class Counter extends Thread {
+    public int count(File file) {
+        int count = 0;
+        try {
+            System.out.println("Counting Words");
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            String line = br.readLine();
+            count = 0;
+            while (line != null) {
+                String[] parts = line.split(" ");
+                for (String w : parts) {
+                    count++;
+                }
+                line = br.readLine();
+            }
+            System.out.println(count);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return count;
+    }
+}
